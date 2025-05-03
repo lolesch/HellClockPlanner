17ea67b71ec32a7a244f442a4b122d0b39c92ece
lolesch <leonid.schreiber@yahoo.de>
2025-05-04 01:48:21 +0200

    [#] new build

--- docs/media/HellClockBuilder/Build/HellClockBuilder.loader.js
+++ docs/media/HellClockBuilder/Build/HellClockBuilder.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 96703462,
+    wasmFileSize: 76336751,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
